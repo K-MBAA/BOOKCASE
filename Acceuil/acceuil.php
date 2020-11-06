d5e2c4bd636e3e6e1232139652f1cf7602102880
K-MBAA <74073837+K-MBAA@users.noreply.github.com>
2020-11-06 21:00:35 +0100

    Create acceuil.php

---
+++ Acceuil/acceuil.php
@@ -0,0 +1,39 @@
+<?php
+require_once('Connection.php');
+include ('menu.php');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Acceuil Bookcase</title>
+</head>
+<body>
+
+	<style>
+		body{
+			background-image: url('pip5.png');
+		}
+		h1{
+			text-align:center;
+			color:white;
+			
+
+		}
+		#bienvenue{
+ 	text-align: center;
+ }
+
+	</style>
+	<div id="global">
+		<div id="bienvenue">
+			<br><br><br/><br/><br/><br/></br>
+			<p><h1>Bonjour , Soyez la Bienvenue ....</h1></p>
+			
+		</div>
+	</div>
+
+
+	
+
+</body>
+</html>
